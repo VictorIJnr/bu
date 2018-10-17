aadd90d11872b939864ae841d741f62ff70b221b
VictorIJnr <victorijnr@gmail.com>
2018-10-17 03:12:10 +0100

    Some netlify stuff and a mini frontend
    Only committing to extract the server folder

--- server/server.js
+++ server/server.js
@@ -1,17 +1,25 @@
 let express = require("express");
-var path = require('path');
-var cookieParser = require('cookie-parser');
+var path = require("path");
+var cookieParser = require("cookie-parser");
+let serverless = require("serverless-http");
 let app = express();
 
+let index = require("./routes/index");
 let data = require("./routes/data");
 
 app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use("/", index);
 app.use("/data", data);
 
 console.log("Running on port");
 
 app.listen(20793);
+
+module.exports.handler = serverless(app);
