122c72cffccd3a13b189a3bc7be0c27032b081cd
VictorIJnr <victorijnr@gmail.com>
2019-01-22 01:42:32 +0000

    I'm THIS close to finishing the AE
    But there's an absolutely fantastic dimension bug. Like it expects the
    input to be 3D, when it's only 2D. But I can't make the ConvNet 2D
    because it has to be 3D, even though it's a 1D convolution. Like, what?
    You can't be serious. What do you expect me to do?

--- src/characterisation/reduction.py
+++ src/characterisation/reduction.py
@@ -4,6 +4,8 @@ from keras.models import Model
 from keras.datasets import mnist
 from keras.callbacks import TensorBoard
 
+from characterisation.classification import sklearnHelper as skh
+
 import numpy as np
 
 """
@@ -12,37 +14,27 @@ Create the AutoEncoder model which will perform feature set reduction
 factor dictates the amount of dimension reduction
 """                
 def initAE(xTrain, xTest, factor=16):
-    #Reduce from 12 dimensions to 3
-    (x_train, _), (x_test, _) = mnist.load_data()
-
-    #This was only for MNIST
-    """ x_train = x_train.astype('float32') / 255.
-    x_test = x_test.astype('float32') / 255.
-    x_train = np.reshape(x_train, (len(x_train), 28, 28, 1)) 
-    x_test = np.reshape(x_test, (len(x_test), 28, 28, 1)) """
-
     numFeatures = xTrain.shape[0]
+    numUsers = xTrain.shape[1]
 
-    print(xTrain.shape / factor)
-
-    inputText = Input(shape=(numFeatures,))
-    x = Conv1D(3, 3, activation="relu", padding="same")(inputText)
-    encoded = MaxPooling1D(2, padding="same")(x)
-
+    inputLayer = Input(shape=(numFeatures,))
+    encoder = initEncoder(xTrain.shape, factor)
     print("Encoding done")
 
-    xDec = UpSampling1D(2)(encoded)
-    decoded = Conv1D(1, 3, activation="sigmoid", padding="same")(xDec)
+    decoder = initDecoder(xTrain.shape, factor)
     print("Decoding done")
 
-    convAuto = Model(inputText, decoded)
+    print(encoder.input.shape)
+
+    convAuto = decoder(encoder.output)
+    convAuto = Model(encoder.input, convAuto)
     convAuto.compile(optimizer="adadelta", loss="binary_crossentropy")
     convAuto.save("autoencoder1D.h5")
 
-    convAuto.fit(x_train, x_train,
-                    epochs=50, batch_size=128,
+    convAuto.fit(xTrain, xTrain,
+                    epochs=50, batch_size=64,
                     shuffle=True,
-                    validation_data=(x_test, x_test),
+                    validation_data=(xTest, xTest),
                     callbacks=[TensorBoard(log_dir="/tmp/autoencoder")])
 
     return convAuto
@@ -51,15 +43,19 @@ def initAE(xTrain, xTest, factor=16):
 """
 Creates the network responsible for encoding data
 """
-def initEncoder(numFeatures, factor=16):
+def initEncoder(trainShape, factor=16):
+    numFeatures = trainShape[0]
+    numUsers = trainShape[1]
+
     reducedSize = numFeatures / factor
 
     #Deep AutoEncoding, scaling down by factor over the course of 4 layers
-    inputLayer = Input(shape=(numFeatures,))
+    #16 is just a hyper-parameter for the number of time steps in the ConvNet
+    inputLayer = Input(shape=(numFeatures, 1))
     xEnc = Conv1D(8, 4, activation="relu", padding="same")(inputLayer)
-    xEnc = MaxPooling1D(factor / 2, padding="same")(xEnc)
+    xEnc = MaxPooling1D(int(factor / 2), padding="same")(xEnc)
     xEnc = Conv1D(4, 4, activation="relu", padding="same")(xEnc)
-    xEnc = MaxPooling1D(factor / 2, padding="same")(xEnc)
+    xEnc = MaxPooling1D(int(factor / 2), padding="same")(xEnc)
 
     #Mapping a user's features to their reduced representation
     encoder = Model(inputLayer, xEnc)
@@ -68,18 +64,24 @@ def initEncoder(numFeatures, factor=16):
 """
 Creates the network responsible for decoding data
 """
-def initDecoder(numFeatures, factor=16):
-    reducedSize = numFeatures / factor
+def initDecoder(trainShape, factor=16):
+    numFeatures = trainShape[0]
+    numUsers = trainShape[1]
 
-    inputLayer = Input(shape=(reducedSize,))
+    reducedSize = int(numFeatures / factor)
+
+    #4 is just calculated from performing the encoding reduction from 16
+    inputLayer = Input(shape=(reducedSize, 4))
     xDec = Conv1D(4, 4, activation="relu", padding="same")(inputLayer)
-    xDec = UpSampling1D(factor / 2)(xDec)
+    xDec = UpSampling1D(int(factor / 2))(xDec)
     xDec = Conv1D(8, 4, activation="relu")(xDec)
-    xDec = UpSampling1D(factor / 2)(xDec)
+    xDec = UpSampling1D(int(factor / 2))(xDec)
     xDec = Conv1D(1, 4, activation="relu")(xDec)
 
     decoder = Model(inputLayer, xDec)
     return decoder
 
 if __name__ == "__main__":
-    initEncoder()
+    xTrain, _, xTest, _ = skh.pullData()
+
+    initAE(xTrain, xTest)
