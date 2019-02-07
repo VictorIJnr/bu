7b81d309bf4f7c710913ba9a22ec006a3d5db523
VictorIJnr <victorijnr@gmail.com>
2019-02-07 11:43:04 +0000

    Added a ConvAE experiment for loss functions
    There's a bunch of graphs on how they all compare too :D

--- src/characterisation/experiments/convAE.py
+++ src/characterisation/experiments/convAE.py
@@ -2,26 +2,51 @@
 
 from argparse import ArgumentParser
 from collections import defaultdict
+from pprint import pprint
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+from matplotlib import pyplot as plt
 
 from helpers import fileIO
 
 from characterisation.helpers.reduction import initDatasetAE
 
 def main():
-    histories = {}
+    histories = []
     lossFuncs = ["mean_squared_error", "mean_absolute_error", "mean_absolute_percentage_error",
                     "mean_squared_logarithmic_error", "squared_hinge", "hinge"]
     
     for lossy in lossFuncs:
-        histories[lossy] = initDatasetAE(loss=lossy, returnHistory=True)[1].history
+        history = initDatasetAE(loss=lossy, returnHistory=True)[1].history
 
-    print(histories)
-    print(type(histories))
+        for i in range(1, 11):
+            lossResults = defaultdict(lambda: {})
+            lossResults["epoch"] = i
+            lossResults["loss_function"] = lossy
+            lossResults["val_loss"] = history["val_loss"][i - 1]
+            lossResults["val_acc"] = history["val_acc"][i - 1]
+            lossResults["loss"] = history["loss"][i - 1]
+            lossResults["acc"] = history["acc"][i - 1]
+
+            histories.append(lossResults)
 
     fileIO.saveJSON(histories, "expConvAE.json")    
 
 def makeGraphs():
-    pass
+    data = fileIO.loadJSON("expConvAE.json")
+
+    myDF = pd.DataFrame(data)
+    myDF = myDF[(myDF.loss_function != "mean_absolute_percentage_error")
+                & (myDF.loss_function != "mean_squared_error")]
+ 
+    liney = sns.lineplot(x="epoch", y="loss", hue="loss_function", data=myDF)
+    liney = liney.get_figure()
+
+    liney.savefig("MaxLossMAE.png")
+    plt.show()
 
 if __name__ == "__main__":
     myParser = ArgumentParser()
