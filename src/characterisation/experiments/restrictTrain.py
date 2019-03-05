0b79e53a9e1a919df65e188c707d7cc405990028
VictorIJnr <victorijnr@gmail.com>
2019-03-05 15:35:08 +0000

    More SSH Debugging

--- src/characterisation/experiments/restrictTrain.py
+++ src/characterisation/experiments/restrictTrain.py
@@ -21,6 +21,71 @@ from helpers import fileIO
 
 savePath = os.path.join(dataPath, "experiments")
 
+
+def formatCVResults(cvResults):
+    #Removing attributes surplus to our accuracy tests.
+    del cvResults["mean_fit_time"]
+    del cvResults["std_fit_time"]
+    del cvResults["mean_score_time"]
+    del cvResults["std_score_time"]
+    del cvResults["split0_test_score"]
+    del cvResults["split1_test_score"]
+    del cvResults["split2_test_score"]
+    del cvResults["split3_test_score"]
+    del cvResults["split4_test_score"]
+    del cvResults["split0_train_score"]
+    del cvResults["split1_train_score"]
+    del cvResults["split2_train_score"]
+    del cvResults["split3_train_score"]
+    del cvResults["split4_train_score"]
+
+    cvResults = pd.DataFrame(cvResults)
+    return cvResults.sort_values("rank_test_score")
+
+def runAccuracyTests(myModel, myDF, mini=False, dataDF=None):
+    accResults = []
+        
+    _, _, xTest, yTest = split(myDF=dataDF)
+
+    for equivTest in testEquivs(myModel, xTest, yTest):
+        accResults.append(equivTest)
+    
+    accResults = pd.DataFrame(accResults)
+    return accResults.fillna(0)
+
+def testEquivs(myModel, xTest, yTest):
+    equivResults = []
+
+    for equiv in Equivs:
+        print(f"Testing the {equiv.name} equivalence class.")
+        modelResults = defaultdict(lambda: {})
+        testDF = expPredict(myModel, xTest, yTest, equivClass=equiv, individual=True)
+
+        equivResults.append(testDF)
+
+        # #Loop over the equivalence class sizes I guess
+        # equivSizes = [len(equivClass) for equivClass in equivClasses]
+        # equivSizeCounter = Counter(equivSizes)
+
+        # modelResults["Equivalence Method"] = equiv.name
+        # modelResults["User Accuracy"] = indAccuracy
+        # modelResults["Class Accuracy"] = claccuracy
+        # modelResults["Min Class Size"] = np.amin(equivSizes)
+        # modelResults["Max Class Size"] = np.amax(equivSizes)
+        # modelResults["Mean Class Size"] = np.mean(equivSizes)
+        # modelResults["Largest Possible Class Size"] = len(np.unique(yTest))
+
+        # for i in np.arange(1, np.amax(equivSizes) + 1):
+        #     modelResults[f"Class Size {i} Count"] = equivSizeCounter[i]
+
+        # if modelParams is not None:
+        #     for paramName, paramValue in modelParams.items():
+        #         modelResults[paramName] = paramValue
+
+        # equivResults.append(dict(modelResults))
+    
+    return equivResults
+
 # Need to create a DF which shows the accuracy related to each equivalence class,
 # For each prediction, show the equivalence class size, the predicted equivalence class,
 # the actual class, and a boolean of whether the predicted class was within the equiv class
@@ -93,67 +158,3 @@ if __name__ == "__main__":
 
     if myArgs.stoppies:
         stoppies(myArgs)
-
-def formatCVResults(cvResults):
-    #Removing attributes surplus to our accuracy tests.
-    del cvResults["mean_fit_time"]
-    del cvResults["std_fit_time"]
-    del cvResults["mean_score_time"]
-    del cvResults["std_score_time"]
-    del cvResults["split0_test_score"]
-    del cvResults["split1_test_score"]
-    del cvResults["split2_test_score"]
-    del cvResults["split3_test_score"]
-    del cvResults["split4_test_score"]
-    del cvResults["split0_train_score"]
-    del cvResults["split1_train_score"]
-    del cvResults["split2_train_score"]
-    del cvResults["split3_train_score"]
-    del cvResults["split4_train_score"]
-
-    cvResults = pd.DataFrame(cvResults)
-    return cvResults.sort_values("rank_test_score")
-
-def runAccuracyTests(myModel, myDF, mini=False, dataDF=None):
-    accResults = []
-        
-    _, _, xTest, yTest = split(myDF=dataDF)
-
-    for equivTest in testEquivs(myModel, xTest, yTest):
-        accResults.append(equivTest)
-    
-    accResults = pd.DataFrame(accResults)
-    return accResults.fillna(0)
-
-def testEquivs(myModel, xTest, yTest):
-    equivResults = []
-
-    for equiv in Equivs:
-        print(f"Testing the {equiv.name} equivalence class.")
-        modelResults = defaultdict(lambda: {})
-        testDF = expPredict(myModel, xTest, yTest, equivClass=equiv, individual=True)
-
-        equivResults.append(testDF)
-
-        # #Loop over the equivalence class sizes I guess
-        # equivSizes = [len(equivClass) for equivClass in equivClasses]
-        # equivSizeCounter = Counter(equivSizes)
-
-        # modelResults["Equivalence Method"] = equiv.name
-        # modelResults["User Accuracy"] = indAccuracy
-        # modelResults["Class Accuracy"] = claccuracy
-        # modelResults["Min Class Size"] = np.amin(equivSizes)
-        # modelResults["Max Class Size"] = np.amax(equivSizes)
-        # modelResults["Mean Class Size"] = np.mean(equivSizes)
-        # modelResults["Largest Possible Class Size"] = len(np.unique(yTest))
-
-        # for i in np.arange(1, np.amax(equivSizes) + 1):
-        #     modelResults[f"Class Size {i} Count"] = equivSizeCounter[i]
-
-        # if modelParams is not None:
-        #     for paramName, paramValue in modelParams.items():
-        #         modelResults[paramName] = paramValue
-
-        # equivResults.append(dict(modelResults))
-    
-    return equivResults
